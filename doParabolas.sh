bd9136f2a153090358c2a43e565351df32f30725
André Torres <torres@ipp.cas.cz>
2021-01-06 11:36:38 +0100

    videos funcionam em windows

--- doParabolas.sh
+++ doParabolas.sh
@@ -1,7 +1,8 @@
 mkdir -p "$1"
 python3 parabolas.py "$1" "$2" "$3" "$4" "$5"
 echo "making video"
-ffmpeg -r 30  -f image2 -i "$1/frame_%04d.png" -vcodec libx264 -crf 25 "$1/$1.mp4"
+rm "$1/$1.mp4"
+ffmpeg -r 30  -f image2 -i "$1/frame_%04d.png" -vcodec libx264 -pix_fmt yuv420p -crf 25 "$1/$1.mp4"
 echo "removing files"
 rm "$1"/frame*
 echo "DONE"
