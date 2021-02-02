ecd90e0cd1f0473cc84d6b935f1065b13478e2d3
André Torres <torres@ipp.cas.cz>
2021-02-02 18:00:41 +0100

    bolas removidas

--- multipleQuadraticas.sh
+++ multipleQuadraticas.sh
@@ -1,20 +1,20 @@
-#./do.sh polinomial quadratica03 1 0 0 -e 2 -r  -f"1 3 2" -p"3 2 1 .,."
-#./do.sh polinomial quadratica23 1 0 0 -e 2 -r  -f"1 3 2" -p"3 2 1 .,." -a
+./do.sh polinomial quadratica03 1 0 0 -e 2 -r  -f"1 3 2" -p"3 2 1 .,."
+./do.sh polinomial quadratica23 1 0 0 -e 2 -r  -f"1 3 2" -p"3 2 1 .,." -a
 ./do.sh polinomial quadratica04 -1 0 0 -e 2 -r  -f"-1 3 2" -p"3 2 1 .,."
 ./do.sh polinomial quadratica24 -1 0 0 -e 2 -r  -f"-1 3 2" -p"3 2 1 .,." -a
-#
-#./do.sh polinomial quadratica06 1 0 0 -e 2  -f"0.5 0 0" -f"2 0 0" -f"-1 0 0" -f"-0.5 0 0" -f"-2 0 0" -l -d5
-#
-#./do.sh polinomial quadratica07 1 0 0 -e 2 -r  -f"1 3 0" -p"3 0 1 .,-"
-#./do.sh polinomial quadratica27 1 0 0 -e 2 -r  -a -f"1 3 0" -p"3 0 1 .,-"
+
+./do.sh polinomial quadratica06 1 0 0 -e 2  -f"0.5 0 0" -f"2 0 0" -f"-1 0 0" -f"-0.5 0 0" -f"-2 0 0" -l -d5 
+
+./do.sh polinomial quadratica07 1 0 0 -e 2 -r  -f"1 3 0" -p"3 0 1 .,-"
+./do.sh polinomial quadratica27 1 0 0 -e 2 -r  -a -f"1 3 0" -p"3 0 1 .,-"
 ./do.sh polinomial quadratica08 -1 0 0 -e 2 -r  -f"-1 3 0" -p"3 0 1 .,-"
 ./do.sh polinomial quadratica28 -1 0 0 -e 2 -r  -a -f"-1 3 0" -p"3 0 1 .,-"
-#
-#./do.sh polinomial quadratica09 1 0 0 -e 2 -r  -f"1 3 0" -f"1 -3 0" -p"3 0 1 .,-" -p"-3 0 2 .,-"
-#./do.sh polinomial quadratica29 1 0 0 -e 2 -r -a -f"1 3 0" -f"1 -3 0" -p"3 0 1 .,-" -p"-3 0 2 .,-"
-#
-#./do.sh polinomial quadratica11 1 0 0 -e 2 -d3   -f"1 0 2" -f"1 0 -2" -p"0 2 1 -,." -p"0 -2 2 -,."
-#./do.sh polinomial quadratica31 1 0 0 -e 2 -d3   -f"1 0 2" -f"1 0 -2" -p"0 2 1 -,." -p"0 -2 2 -,." -a
-#
+
+./do.sh polinomial quadratica09 1 0 0 -e 2 -r  -f"1 3 0" -f"1 -3 0" -p"3 0 1 .,-" -p"-3 0 2 .,-"
+./do.sh polinomial quadratica29 1 0 0 -e 2 -r -a -f"1 3 0" -f"1 -3 0" -p"3 0 1 .,-" -p"-3 0 2 .,-"
+
+./do.sh polinomial quadratica11 1 0 0 -e 2 -d3   -f"1 0 2" -f"1 0 -2" -p"0 2 1 -,." -p"0 -2 2 -,."
+./do.sh polinomial quadratica31 1 0 0 -e 2 -d3   -f"1 0 2" -f"1 0 -2" -p"0 2 1 -,." -p"0 -2 2 -,." -a
+
 ./do.sh polinomial quadratica12 -1 0 0 -e 2 -d3   -f"-1 0 2" -f"-1 0 -2" -p"0 2 1 -,." -p"0 -2 2 -,."
 ./do.sh polinomial quadratica32 -1 0 0 -e 2 -d3  -a -f"-1 0 2" -f"-1 0 -2" -p"0 2 1 -,." -p"0 -2 2 -,."
