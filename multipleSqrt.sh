448aae5f63217fc7b8c883ea58ec85a35cc136f3
Andre Torres <andregtorres@gmail.com>
2021-04-08 21:20:00 +0200

    add sqrt

---
+++ multipleSqrt.sh
@@ -0,0 +1,7 @@
+#./do.sh sqrt sqrt01 1 0 0 -r -d4
+#./do.sh sqrt sqrt02 1 0 0 -r -a -f"1 3 0" -p"3 0 1 -,-" -d4
+#./do.sh sqrt sqrt03 1 0 0 -r -f"2 3 0" -p"3 0 1 -,-" -d4
+#./do.sh sqrt sqrt04 1 0 0 -r -a -f"2 3 2" -p"3 2 1 -,-" -d4
+#./do.sh sqrt sqrt05 1 0 0 -r -a -f"1 3 0" -p"3 0 1 .,-" -d4
+./do.sh sqrt sqrt06 1 0 0 -r -f"2 3 0" -p"3 0 1 .,-" -d4
+./do.sh sqrt sqrt07 1 0 0 -r -a -f"2 3 1" -p"3 1 1 .,." -d4
